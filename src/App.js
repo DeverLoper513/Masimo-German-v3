57bbf93d2cf05846c2a343f457273f7e5dcba9a9
DeverLoper513 <151453624+DeverLoper513@users.noreply.github.com>
2024-08-20 21:49:32 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "494907";
-  let InstanceId_QA = "";
+  let InstanceId_Widget = "821889";
+  let InstanceId_QA = "835234";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "";
+  let InstanceId_StarRating = "835191";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "8676443062573";
+  let ProductId = "";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
